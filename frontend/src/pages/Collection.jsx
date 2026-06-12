28eebfb93f20e24e765fb9c428a8a72ad1efc98e
Rishav Raj <rishav.raj.mec23@itbhu.ac.in>
2026-06-13 00:26:31 +0530

    added logic for 'sort by' feature

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -10,6 +10,7 @@ const Collection = () => {
   const [filterProducts,setFilterProducts] = useState([]);
   const [category,setCategory] = useState([]);
   const [subCategory,setSubCategory] = useState([]);
+  const [sortType,setSortType] = useState('relavent')
 
   const toggleCategory = (e) => {
     if(category.includes(e.target.value)) {
@@ -41,6 +42,21 @@ const Collection = () => {
     setFilterProducts(productsCopy)
   }
 
+  const sortProduct = () => {
+    let fpCopy = filterProducts.slice();
+    switch (sortType) {
+      case 'low-high' :
+        setFilterProducts(fpCopy.sort((a,b)=>(a.price - b.price)));
+        break;
+      case 'high-low' :
+        setFilterProducts(fpCopy.sort((a,b)=>(b.price - a.price)));
+        break;
+      default:
+        applyFilter();
+        break;
+    }
+  }
+
   // useEffect(()=>{
   //   setFilterProducts(products);
   // },[products])
@@ -48,6 +64,10 @@ const Collection = () => {
   useEffect(()=>{
     applyFilter();
   },[category,subCategory])
+
+  useEffect(()=>{
+    sortProduct();
+  },[sortType])
 
   useEffect(()=>{
     console.log(category);
@@ -101,7 +121,7 @@ const Collection = () => {
         <div className='flex justify-between text-base sm:text-2xl mb-4'>
           <Title text1={'ALL'} text2={'COLLECTIONS'} />
           {/* Product sort  */}
-          <select className='border-2 border-gray-300 text-sm px-2'>
+          <select onChange={(e)=>setSortType(e.target.value)} className='border-2 border-gray-300 text-sm px-2'>
             <option value="relavent">Sort by: Relavent</option>
             <option value="low-high">Sort by: Low to High</option>
             <option value="high-low">Sort by: High to Low</option>
