f3bd987dc21bb570034f9d4968d68154b50b1357
Rishav Raj <rishav.raj.mec23@itbhu.ac.in>
2026-06-12 12:58:37 +0530

    created footer and completed UI of home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ import Product from './pages/Product'
 import Contact from './pages/Contact'
 import About from './pages/About'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 const App = () => {  
   return (
@@ -26,6 +27,7 @@ const App = () => {
         <Route path='/place-order' element={<PlaceOrder/>} />
         <Route path='/orders' element={<Orders/>} />
       </Routes>
+      <Footer />
       
     </div>
   )
