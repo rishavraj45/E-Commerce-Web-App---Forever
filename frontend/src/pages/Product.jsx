0ba04377ae0b2053b238a2b2898a6f4ec7ede3e7
Rishav Raj <rishav.raj.mec23@itbhu.ac.in>
2026-06-14 23:50:21 +0530

    continued: description and review section

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -74,6 +74,20 @@ const Product = () => {
             </div>
         </div>
       </div>
+
+      {/* Description and review section  */}
+      <div className='mt-20'>
+        <div className='flex'>
+          <b className='border px-5 py-3 text-sm'>Description</b>
+          <p className='border px-5 py-3 text-sm'>Reviews (122)</p>
+        </div>
+        <div className='flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500'>
+          <p>An e-commerce website is an online platform that facilitates the buying and selling of products or services over the internet. It serves as a virtual marketplace where businesses and individuals can showcase their products, interact with customers, and conduct transactions without the need for a physical presence. E-commerce websites have gained immense popularity due to their convenience, accessibility, and the global reach they offer.</p>
+          <p>E-commerce websites typically display products or services along with detailed descriptions, images, prices, and any available variations (e.g., sizes, colors). Each product usually has its own dedicated page with relevant information.</p>
+        </div>
+      </div>
+
+      {/* display related products  */}
     </div>
   ) : <div className='opacity-0'></div>
 }
