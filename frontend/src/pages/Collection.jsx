372e6ab72045f5f1334275321e8daf40ea26b1d0
Rishav Raj <rishav.raj.mec23@itbhu.ac.in>
2026-06-14 22:39:17 +0530

    added logic to search component

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -5,7 +5,7 @@ import Title from '../components/Title';
 import ProductItem from '../components/ProductItem';
 
 const Collection = () => {
-  const { products } = useContext(ShopContext);
+  const { products, search, showSearch } = useContext(ShopContext);
   const [showFilter,setShowFilter] = useState(false);
   const [filterProducts,setFilterProducts] = useState([]);
   const [category,setCategory] = useState([]);
@@ -32,6 +32,10 @@ const Collection = () => {
 
   const applyFilter = () => {
     let productsCopy = products.slice();
+
+    if(showSearch && search){
+      productsCopy = productsCopy.filter(item=> item.name.toLowerCase().includes(search.toLowerCase()));
+    }
     if(category.length >0) {
       productsCopy = productsCopy.filter(item => category.includes(item.category));
     }
@@ -70,7 +74,7 @@ const Collection = () => {
 
   useEffect(()=>{
     applyFilter();
-  },[category,subCategory,sortType,products])
+  },[category,subCategory,sortType,products,search,showSearch])
 
   // useEffect(()=>{
   //   sortProduct();
