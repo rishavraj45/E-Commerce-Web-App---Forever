984e7ecc6b7295d39d51b88c58cef2ebac627ad4
Rishav Raj <rishav.raj.mec23@itbhu.ac.in>
2026-06-15 22:50:58 +0530

    products page ui complete : added related products section

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { ShopContext } from '../context/ShopContext';
 import { assets } from '../assets/assets';
+import RelatedProducts from '../components/RelatedProducts';
 
 const Product = () => {
 
@@ -88,6 +89,7 @@ const Product = () => {
       </div>
 
       {/* display related products  */}
+      <RelatedProducts category={productData.category} subCategory={productData.subCategory} />
     </div>
   ) : <div className='opacity-0'></div>
 }
