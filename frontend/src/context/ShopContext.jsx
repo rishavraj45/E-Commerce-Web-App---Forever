4533eb6b5c38a36b0eccea544730c87346fb2935
Rishav Raj <rishav.raj.mec23@itbhu.ac.in>
2026-06-15 23:05:32 +0530

    addToCart functionality

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
 
 export const ShopContext = createContext();
@@ -8,10 +8,34 @@ const ShopContextProvider = (props) => {
     const delievery_fee = 10;
     const [search,setSearch] = useState('');
     const [showSearch,setShowSearch] = useState(false); //true then display the search bar
+    const [cartItems,setCartItems] = useState({});
+
+    const addToCart = async (itemId,size) => {
+        let cartData = structuredClone(cartItems);  //creating copy of the object
+        if(cartData[itemId]) {
+            if(cartData[itemId][size]) {
+                cartData[itemId][size] +=1;
+            }
+            else {
+                cartData[itemId][size] =1;
+            }
+        }
+        else {
+            cartData[itemId] = {};
+            cartData[itemId][size] =1;
+        }
+        setCartItems(cartData);
+
+    }
+
+    useEffect(()=>{
+        console.log(cartItems)
+    },[cartItems])
 
     const value = {
         products , currency , delievery_fee ,
-        search , setSearch , showSearch , setShowSearch
+        search , setSearch , showSearch , setShowSearch,
+        cartItems, addToCart
     }
     return (
         <ShopContext.Provider value={value}>
